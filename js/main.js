590e49e4d5385d42daf9983602a17045d9f771b8
Jeltok <sinkvane@gmail.com>
2021-05-21 19:44:14 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
+  slideToClickedSlide: true,
+  centeredslides: true,
+  height: 500,
+  spaceBetween: 0,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    draggable: true
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  mousewheel: {
+    invert: true
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1
+    },
+    992: {
+      slidesPerView: 2
+    },
+    1799: {
+      slidesPerView: 3
+    }
+  }
+});
+//# sourceMappingURL=main.js.map
